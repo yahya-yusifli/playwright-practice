391fc4f4f96792e1cb3c5e949b5776f31f8db60b
yahya <yahya_yusifli@outlook.com>
2025-12-15 18:46:24 -0500

    add GS

--- tests/saucedemo-login.spec.ts
+++ tests/saucedemo-login.spec.ts
@@ -2,8 +2,8 @@ import { test, expect } from "@playwright/test";
 
 test("login to Sauce Demo", async ({ page }) => {
   await page.goto("/");
-  await page.getByPlaceholder("Username").fill(process.env.TEST_USERNAME!);
-  await page.getByPlaceholder("Password").fill(process.env.TEST_PASSWORD!);
+  await page.getByPlaceholder("Username").fill(process.env.SAUCE_USERNAME!);
+  await page.getByPlaceholder("Password").fill(process.env.SAUCE_PASSWORD!);
   await page.getByRole("button", { name: "Login" }).click();
 
   await page.waitForURL("**/inventory.html");
