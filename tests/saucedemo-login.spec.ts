cbd8eb7dfb6b0ec02775bab10b4ae9178b94cb39
yahya <yahya_yusifli@outlook.com>
2025-12-15 19:01:26 -0500

    add BASE_URL

--- tests/saucedemo-login.spec.ts
+++ tests/saucedemo-login.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("login to Sauce Demo", async ({ page }) => {
-  await page.goto("/");
+  await page.goto(process.env.BASE_URL!);
   await page.getByPlaceholder("Username").fill(process.env.SAUCE_USERNAME!);
   await page.getByPlaceholder("Password").fill(process.env.SAUCE_PASSWORD!);
   await page.getByRole("button", { name: "Login" }).click();
